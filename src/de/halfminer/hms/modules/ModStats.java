eba0868a20f4a6f52ceff9c7b5ada8afc4d16c33
Kakifrucht <Kakifrucht@users.noreply.github.com>
2016-03-12 20:03:25 +0100

    Fix sounds

--- src/de/halfminer/hms/modules/ModStats.java
+++ src/de/halfminer/hms/modules/ModStats.java
@@ -138,10 +138,8 @@ public class ModStats extends HalfminerModule implements Listener {
             }
 
             e.getPlayer().sendMessage(message);
-            e.getPlayer().playSound(clicked.getLocation(), Sound.BLOCK_NOTE_PLING, 1.0f, 1.4f); //TODO correct NOTE_STICKS
-
+            e.getPlayer().playSound(clicked.getLocation(), Sound.BLOCK_SLIME_HIT, 1.0f, 2.0f);
         }
-
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
