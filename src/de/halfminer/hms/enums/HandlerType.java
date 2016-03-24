5bba7b52e8fbcd75f0e20451f8e1843b18d66f74
Kakifrucht <Kakifrucht@users.noreply.github.com>
2016-03-24 04:15:22 +0100

    Fix permissions issue

--- src/de/halfminer/hms/enums/HandlerType.java
+++ src/de/halfminer/hms/enums/HandlerType.java
@@ -2,10 +2,10 @@ package de.halfminer.hms.enums;
 
 public enum HandlerType {
 
-    BOSSBAR("BossBar"),
-    STORAGE ("Storage"),
-    TELEPORT ("Teleport"),
-    TITLES ("Titles");
+    BOSSBAR     ("BossBar"),
+    STORAGE     ("Storage"),
+    TELEPORT    ("Teleport"),
+    TITLES      ("Titles");
 
     private final String className;
 
--- src/de/halfminer/hms/enums/ModuleType.java
+++ src/de/halfminer/hms/enums/ModuleType.java
@@ -2,20 +2,20 @@ package de.halfminer.hms.enums;
 
 public enum ModuleType {
 
-    ANTI_KILLFARMING ("AntiKillfarming"),
-    AUTO_MESSAGE ("AutoMessage"),
-    GLITCH_PROTECTION ("CombatLog"),
-    COMBAT_LOG ("GlitchProtection"),
-    MOTD ("Motd"),
-    PERFORMANCE ("Performance"),
-    PVP ("PvP"),
-    RESPAWN ("Respawn"),
-    SIGN_EDIT ("SignEdit"),
-    SKILL_LEVEL ("SkillLevel"),
-    STATIC_LISTENERS ("StaticListeners"),
-    STATS ("Stats"),
-    TITLES ("Titles"),
-    TPS ("Tps");
+    ANTI_KILLFARMING    ("AntiKillfarming"),
+    AUTO_MESSAGE        ("AutoMessage"),
+    COMBAT_LOG          ("CombatLog"),
+    GLITCH_PROTECTION   ("GlitchProtection"),
+    MOTD                ("Motd"),
+    PERFORMANCE         ("Performance"),
+    PVP                 ("PvP"),
+    RESPAWN             ("Respawn"),
+    SIGN_EDIT           ("SignEdit"),
+    SKILL_LEVEL         ("SkillLevel"),
+    STATIC_LISTENERS    ("StaticListeners"),
+    STATS               ("Stats"),
+    TITLES              ("Titles"),
+    TPS                 ("Tps");
 
     private final String className;
 
