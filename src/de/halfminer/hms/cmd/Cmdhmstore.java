5a038b0d79e8dca90881906934f0dd0f58bee538
Kakifrucht <Kakifrucht@users.noreply.github.com>
2016-04-13 04:32:04 +0200

    Fix hmstore

--- src/de/halfminer/hms/cmd/Cmdhmstore.java
+++ src/de/halfminer/hms/cmd/Cmdhmstore.java
@@ -1,13 +1,17 @@
 package de.halfminer.hms.cmd;
 
+import de.halfminer.hms.enums.DataType;
 import de.halfminer.hms.exception.PlayerNotFoundException;
+import de.halfminer.hms.util.HalfminerPlayer;
 import de.halfminer.hms.util.Language;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-
-import java.util.UUID;
 
 @SuppressWarnings("unused")
 public class Cmdhmstore extends HalfminerCommand {
+
+    private HalfminerPlayer player;
+    private DataType type;
 
     public Cmdhmstore() {
         this.permission = "hms.admin";
@@ -18,27 +22,32 @@ public class Cmdhmstore extends HalfminerCommand {
 
         if (args.length > 1) { // Set and get variables
 
+            player = null;
+            type = null;
             // Replace playername with UUID if found
             String path = args[1].toLowerCase();
             String[] split = path.split("[.]");
             // Only replace if the path is longer than 1 and it is not sys, as this var is reserved
-            if (split.length > 1 && !split[0].equals("sys")) {
+            if (split.length > 1) {
 
                 try {
                     // Get UUID of player
-                    UUID playerUid = storage.getUUID(split[0]);
-                    split[0] = playerUid.toString();
-                    path = "";
-                    for (String str : split) path += str + ".";
-                    path = path.substring(0, path.length() - 1);
-                } catch (PlayerNotFoundException e) {
-                    // Player not found, use lowercase path
+                    player = storage.getPlayer(split[0]);
+                    for (DataType typeQuery : DataType.values()) {
+                        if (typeQuery.toString().equalsIgnoreCase(split[1])) {
+                            type = typeQuery;
+                            break;
+                        }
+                    }
+                    if (type == null) player = null;
+                    else path = ChatColor.GOLD + split[0] + ChatColor.GRAY + ChatColor.ITALIC + '.' + split[1];
+                } catch (PlayerNotFoundException ignored) {
                 }
             }
             if (args.length > 2 && args[0].equalsIgnoreCase("set")) {
 
                 String setTo = Language.arrayToString(args, 2, false);
-                storage.set(path, setTo);
+                set(path, setTo);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreSet", true, "%PREFIX%", "Info",
                         "%PATH%", path, "%VALUE%", setTo));
             } else if (args.length > 2 && args[0].equalsIgnoreCase("setint")) {
@@ -51,29 +60,31 @@ public class Cmdhmstore extends HalfminerCommand {
                     return;
                 }
 
-                storage.set(path, setTo);
+                set(path, setTo);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreSet", true, "%PREFIX%", "Info",
                         "%PATH%", path, "%VALUE%", String.valueOf(setTo)));
             } else if (args.length > 2 && args[0].equalsIgnoreCase("setbool")) {
 
                 boolean setTo = Boolean.parseBoolean(args[2]);
-                storage.set(path, setTo);
+                set(path, setTo);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreSet", true, "%PREFIX%", "Info",
                         "%PATH%", path, "%VALUE%", String.valueOf(setTo)));
             } else if (args.length > 2 && args[0].equalsIgnoreCase("setdouble")) {
 
                 double setTo = Double.parseDouble(args[2]);
-                storage.set(path, setTo);
+                set(path, setTo);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreSet", true, "%PREFIX%", "Info",
                         "%PATH%", path, "%VALUE%", String.valueOf(setTo)));
             } else if (args[0].equalsIgnoreCase("get")) {
 
-                String value = storage.getString(path);
+                String value;
+                if (player == null) value = storage.getString(path);
+                else value = player.getString(type);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreGet", true, "%PREFIX%", "Info",
                         "%PATH%", path, "%VALUE%", value));
             } else if (args[0].equalsIgnoreCase("remove")) {
 
-                storage.set(path, null);
+                set(path, null);
                 sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreRemove", true, "%PREFIX%", "Info",
                         "%PATH%", path));
             } else {
@@ -93,5 +104,10 @@ public class Cmdhmstore extends HalfminerCommand {
                 }
             });
         } else sender.sendMessage(Language.getMessagePlaceholders("commandHmstoreUsage", true, "%PREFIX%", "Info"));
+    }
+
+    private void set(String path, Object setTo) {
+        if (player != null) player.set(type, setTo);
+        else storage.set(path, setTo);
     }
 }
