547b54fd5a8f1a7a655dbf9889a44f22fee2cfa5
Kakifrucht <Kakifrucht@users.noreply.github.com>
2016-05-12 03:48:30 +0200

    Reformat

--- src/de/halfminer/hms/cmd/Cmdhmsapi.java
+++ src/de/halfminer/hms/cmd/Cmdhmsapi.java
@@ -29,104 +29,102 @@ public class Cmdhmsapi extends HalfminerCommand {
 
         if (args.length == 0 || !sender.isOp()) return;
 
-        if (args.length > 0) {
+        if (args[0].equalsIgnoreCase("title") && args.length > 2) {
 
-            if (args[0].equalsIgnoreCase("title") && args.length > 2) {
+            Player sendTo = server.getPlayer(args[1]);
+            if (sendTo != null) {
+                ((HanTitles) hms.getHandler(HandlerType.TITLES)).sendTitle(sendTo,
+                        Language.arrayToString(args, 2, false), 0, 50, 10);
+            }
 
-                Player sendTo = server.getPlayer(args[1]);
-                if (sendTo != null) {
-                    ((HanTitles) hms.getHandler(HandlerType.TITLES)).sendTitle(sendTo,
-                            Language.arrayToString(args, 2, false), 0, 50, 10);
-                }
+        } else if (args[0].equalsIgnoreCase("hasroom") && args.length > 2) {
 
-            } else if (args[0].equalsIgnoreCase("hasroom") && args.length > 2) {
-
-                Player player;
-                try {
-                    OfflinePlayer playerOffline = storage.getPlayer(args[1]).getBase();
-                    if (!(playerOffline instanceof Player)) {
-                        setHasRoomBoolean(playerOffline, false);
-                        return;
-                    }
-                    player = (Player) playerOffline;
-                } catch (PlayerNotFoundException e) {
-                    // shouldn't happen
+            Player player;
+            try {
+                OfflinePlayer playerOffline = storage.getPlayer(args[1]).getBase();
+                if (!(playerOffline instanceof Player)) {
+                    setHasRoomBoolean(playerOffline, false);
                     return;
                 }
+                player = (Player) playerOffline;
+            } catch (PlayerNotFoundException e) {
+                // shouldn't happen
+                return;
+            }
 
-                int freeSlotsRequired;
-                try {
-                    freeSlotsRequired = Integer.decode(args[2]);
-                } catch (NumberFormatException e) {
-                    freeSlotsRequired = 1;
+            int freeSlotsRequired;
+            try {
+                freeSlotsRequired = Integer.decode(args[2]);
+            } catch (NumberFormatException e) {
+                freeSlotsRequired = 1;
+            }
+
+            int freeSlotsCount = 0;
+
+            for (ItemStack stack : player.getInventory().getStorageContents())
+                if (stack == null) freeSlotsCount++;
+
+            setHasRoomBoolean(player, freeSlotsCount >= freeSlotsRequired);
+
+        } else if (sender instanceof Player) {
+
+            Player player = (Player) sender;
+            ConsoleCommandSender consoleInstance = server.getConsoleSender();
+
+            if (args[0].equalsIgnoreCase("takecase")) {
+
+                String playername = player.getName();
+
+                if (player.getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) {
+
+                    ItemStack hand = player.getInventory().getItemInMainHand();
+
+                    server.dispatchCommand(consoleInstance, "vt setstr temp casename_"
+                            + sender.getName() + " " + hand.getItemMeta().getDisplayName());
+                    int amount = hand.getAmount();
+                    if (amount > 1) hand.setAmount(hand.getAmount() - 1);
+                    else player.getInventory().setItemInMainHand(null);
+                    server.dispatchCommand(consoleInstance, "vt run casino:caseopen " + playername);
+
+                } else {
+
+                    server.dispatchCommand(consoleInstance, "vt run casino:error " + playername);
                 }
 
-                int freeSlotsCount = 0;
+            } else if (args[0].equalsIgnoreCase("takehead")) {
 
-                for (ItemStack stack : player.getInventory().getStorageContents())
-                    if (stack == null) freeSlotsCount++;
+                ItemStack item = player.getInventory().getItemInMainHand();
+                if ((item != null) && (item.getType() == Material.SKULL_ITEM)) {
 
-                setHasRoomBoolean(player, freeSlotsCount >= freeSlotsRequired);
-
-            } else if (sender instanceof Player) {
-
-                Player player = (Player) sender;
-                ConsoleCommandSender consoleInstance = server.getConsoleSender();
-
-                if (args[0].equalsIgnoreCase("takecase")) {
-
-                    String playername = player.getName();
-
-                    if (player.getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) {
-
-                        ItemStack hand = player.getInventory().getItemInMainHand();
-
-                        server.dispatchCommand(consoleInstance, "vt setstr temp casename_" + sender.getName() + " " + hand.getItemMeta().getDisplayName());
-                        int amount = hand.getAmount();
-                        if (amount > 1) hand.setAmount(hand.getAmount() - 1);
-                        else player.getInventory().setItemInMainHand(null);
-                        server.dispatchCommand(consoleInstance, "vt run casino:caseopen " + playername);
-
-                    } else {
-
-                        server.dispatchCommand(consoleInstance, "vt run casino:error " + playername);
+                    SkullMeta skull = (SkullMeta) item.getItemMeta();
+                    if (!skull.hasOwner()) {
+                        server.dispatchCommand(consoleInstance, "vt run casino:error " + player.getName());
+                        return;
                     }
 
-                } else if (args[0].equalsIgnoreCase("takehead")) {
 
-                    ItemStack item = player.getInventory().getItemInMainHand();
-                    if ((item != null) && (item.getType() == Material.SKULL_ITEM)) {
+                    String skullOwner = skull.getOwner();
 
-                        SkullMeta skull = (SkullMeta) item.getItemMeta();
-                        if (!skull.hasOwner()) {
-                            server.dispatchCommand(consoleInstance, "vt run casino:error " + player.getName());
-                            return;
-                        }
+                    int level;
+                    try {
+                        level = storage.getPlayer(skullOwner).getInt(DataType.SKILL_LEVEL);
+                    } catch (PlayerNotFoundException e) {
+                        level = 1;
+                    }
 
+                    server.dispatchCommand(consoleInstance,
+                            "vt setstr temp headname_" + player.getName() + " " + skullOwner);
+                    server.dispatchCommand(consoleInstance,
+                            "vt setint temp headlevel_" + player.getName() + " " + String.valueOf(level));
 
-                        String skullOwner = skull.getOwner();
+                    // Remove the skull
+                    int amount = item.getAmount();
+                    if (amount > 1) item.setAmount(amount - 1);
+                    else player.getInventory().setItemInMainHand(null);
+                    // proceed with next step
+                    server.dispatchCommand(consoleInstance, "vt run casino:roulette " + player.getName());
 
-                        int level;
-                        try {
-                            level = storage.getPlayer(skullOwner).getInt(DataType.SKILL_LEVEL);
-                        } catch (PlayerNotFoundException e) {
-                            level = 1;
-                        }
-
-                        server.dispatchCommand(consoleInstance,
-                                "vt setstr temp headname_" + player.getName() + " " + skullOwner);
-                        server.dispatchCommand(consoleInstance,
-                                "vt setint temp headlevel_" + player.getName() + " " + String.valueOf(level));
-
-                        // Remove the skull
-                        int amount = item.getAmount();
-                        if (amount > 1) item.setAmount(amount - 1);
-                        else player.getInventory().setItemInMainHand(null);
-                        // proceed with next step
-                        server.dispatchCommand(consoleInstance, "vt run casino:roulette " + player.getName());
-
-                    } else server.dispatchCommand(consoleInstance, "vt run casino:error " + player.getName());
-                }
+                } else server.dispatchCommand(consoleInstance, "vt run casino:error " + player.getName());
             }
         }
     }
