76ae104b35550a46caf632f3cd31c5b274cc35cd
Kakifrucht <Kakifrucht@users.noreply.github.com>
2017-01-21 03:46:21 +0100

    Test commit

---
+++ HalfminerWebAPI/src/main/java/de/halfminer/hmwapi/HalfminerWebAPI.java
@@ -0,0 +1,20 @@
+package de.halfminer.hmwapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by fabpw on 20.01.2017.
+ */
+public class HalfminerWebAPI extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
